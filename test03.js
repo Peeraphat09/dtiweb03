971940607ee5253e8c480840731467757fe1b141
Peeraphat09 <werqazq48@gmail.com>
2025-08-27 11:06:58 +0700

    module update

---
+++ test03.js
@@ -0,0 +1,10 @@
+const dti03 = require(`./muymodule03.js`)
+// const(sauYY)
+
+console.log(dti03.sauYY)
+console.log(dti03.sauYY)
+console.log(dti03.sumNumberYY(10,20))
+console.log(dti93.squareAreaYY(100,20))
+
+// console.log(sauYY)
+// console.log(sumNumberYY(10,20))
